3f46ae5f31236e1abb1761d8ddc1eec95b2c6b69
David G.Muñoz <72562392+davidgregoriom@users.noreply.github.com>
2024-06-06 19:17:02 +0200

    Update

--- Fresh/Proyectos/Simulacros/Apollo_page/routes/index.tsx
+++ Fresh/Proyectos/Simulacros/Apollo_page/routes/index.tsx
@@ -1,5 +1,62 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import { Request, FreshContext,Handlers,Response} from "$fresh/server.ts";
+
+export const handler:Handlers={
+  GET: async(_req:Request,ctx:FreshContext):Promise<Response>=>{
+    const response = await fetch('https://rickandmortyapi.com/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            characters(page:1){
+              results{
+                id
+                name
+                status
+              }
+            }
+          }
+        `
+      })
+    });
+    console.log(response);
+    console.log(response.json());
+    console.log(Object.keys(response));
+    const data= await response.json();
+    return ctx.render(data);
+  },
+  POST: async (req:Request,ctx:FreshContext):Promise<Response>=>{
+    const formData = await req.formData();
+    const json = await formData.toJSON();
+    const response = await fetch('https://rickandmortyapi.com/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: `
+          mutation ($name: String!, $status: String!) {
+            characters(input.{name: $name, status: $status}){
+              results{
+                id
+                name
+                status
+              }
+            }
+          }
+        `,
+        variables: JSON.stringify(json)
+      })
+    });
+    const data = await response.json();
+    return ctx.render(data);
+  }
+}
+
 
 export default function Home() {
   const count = useSignal(3);
